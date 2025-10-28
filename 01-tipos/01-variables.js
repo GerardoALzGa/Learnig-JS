a0464e5f8e4afbe58821621f7ff0892e9ab5e4b5
Gerardo Lopez <gerardoglg12@gmail.com>
2025-10-28 13:41:28 -0600

    Add new files

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -1,1 +1,8 @@
-let nombre = " "
+UpperCamelCase, camelCase, snake_case
+let nombre = 'Hola Mundo';
+let nombreCompleto;
+let nombre_completo;
+console.log(nombre);
+let apellido;
+let animal;
+apellido = 'Schurmann';
