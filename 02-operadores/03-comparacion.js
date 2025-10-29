f2fb4054f9e5816812275e17866ee3cb07e9bd51
Gerardo Lopez <gerardoglg12@gmail.com>
2025-10-28 22:37:03 -0600

    I'm practice with operators of comparations

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,14 @@
+//RELACIONALES
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//IGUALDAD y solo pregunta por el valor
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10');
+console.log(a === '10');
+console.log(a === '10');// para preguntar si son diferentes tipos de datos
+console.log(a !== '10');
